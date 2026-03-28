ae814cb4be794e3b74eda0deb033c0d06eee95f7
Darshil Shah <darshils99@gmail.com>
2026-03-28 18:59:44 -0500

    fix: resolve React 19 white-screen crash on createRoot

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // To allow tailwind if they eventually add it, though presently handled via CDN in html
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
