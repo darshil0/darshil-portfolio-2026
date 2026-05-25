9a7d45bbd0d69afaaaf06714082e52dec3161628
Darshil Shah <darshils99@gmail.com>
2026-05-25 18:04:59 -0500

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,14 +1,16 @@
 import { defineConfig, devices } from '@playwright/test';
 
+const baseURL = 'http://localhost:5173';
+
 export default defineConfig({
   testDir: './tests/e2e',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [['html', { open: 'never' }]],
   use: {
-    baseURL: 'http://localhost:5173',
+    baseURL,
     trace: 'on-first-retry',
   },
   projects: [
@@ -19,7 +21,7 @@ export default defineConfig({
   ],
   webServer: {
     command: 'npm run dev',
-    url: 'http://localhost:5173',
+    url: baseURL,
     reuseExistingServer: !process.env.CI,
   },
 });
